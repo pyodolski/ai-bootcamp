8dba335b5883b262668a136becb2cd0cf92b6959
pyodolski <pjwp0928w@gmail.com>
2025-03-29 14:48:02 +0900

    step-01

---
+++ step-01/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+using OpenAI;
+using System.ClientModel;
+
+
+var config = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                 .AddUserSecrets<Program>()
+                 .Build();
+
+var client = new OpenAIClient(
+    credential: new ApiKeyCredential(config["GitHub:Models:AccessToken"]!),
+    options: new OpenAIClientOptions { Endpoint = new Uri(config["GitHub:Models:Endpoint"]!) });
+
+var kernel = Kernel.CreateBuilder()
+                   .AddGoogleAIGeminiChatCompletion(
+                        modelId: config["Google:Gemini:ModelName"]!,
+                        apiKey: config["Google:Gemini:ApiKey"]!,
+                        serviceId: "google")
+                   .Build();
+
+var input = default(string);
+var message = default(string);
+while (true)
+{
+    Console.Write("User: ");
+    input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        break;
+    }
+
+    Console.Write("Assistant: ");
+    Console.WriteLine();
+    Console.WriteLine("--- Response from Google Gemini ---");
+    var responseGoogle = kernel.InvokePromptStreamingAsync(
+            promptTemplate: input,
+            arguments: new KernelArguments(new PromptExecutionSettings() { ServiceId = "google" }));
+    await foreach (var content in responseGoogle)
+    {
+        await Task.Delay(20);
+        message += content;
+        Console.Write(content);
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("--- Response from GitHub Models ---");
+    var responseGH = kernel.InvokePromptStreamingAsync(
+            promptTemplate: input,
+            arguments: new KernelArguments(new PromptExecutionSettings() { ServiceId = "github" }));
+    await foreach (var content in responseGH)
+    {
+        await Task.Delay(20);
+        message += content;
+        Console.Write(content);
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
